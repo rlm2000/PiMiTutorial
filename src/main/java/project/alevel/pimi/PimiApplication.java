a1f6040b5c14af6955ab4f0cca17a79297b83de0
Rebecca Marsey <rlmarsey@gmail.com>
2018-08-05 22:25:05 +0100

    Initial Spring Boot Project

---
+++ src/main/java/project/alevel/pimi/PimiApplication.java
@@ -0,0 +1,12 @@
+package project.alevel.pimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimiApplication.class, args);
+	}
+}
---
+++ src/test/java/project/alevel/pimi/PimiApplicationTests.java
@@ -0,0 +1,16 @@
+package project.alevel.pimi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PimiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
